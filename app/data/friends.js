da9b2d7f1988a1db0f3ca04e23eb8e8dc64e599f
shar09 <sharathtelu9@gmail.com>
2019-07-07 22:24:49 -0500

    api and html routes to display to display home and survey page working

---
+++ app/data/friends.js
@@ -0,0 +1,275 @@
+var friends = [
+    {
+        "name": "Ahmed",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+        "scores": [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+        ]
+    },
+    
+    {
+    "name": "Jacob Deming",
+    "photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+    "scores": [
+    "4",
+    "2",
+    "5",
+    "1",
+    "3",
+    "2",
+    "2",
+    "1",
+    "3",
+    "2"
+    ]
+    },
+    {
+    "name": "Jeremiah Scanlon",
+    "photo": "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+    "scores": [
+    "5",
+    "2",
+    "2",
+    "2",
+    "4",
+    "1",
+    "3",
+    "2",
+    "5",
+    "5"
+    ]
+    },
+    {
+    "name": "Louis T. Delia",
+    "photo": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+    "scores": [
+    "3",
+    "3",
+    "4",
+    "2",
+    "2",
+    "1",
+    "3",
+    "2",
+    "2",
+    "3"
+    ]
+    },
+    {
+    "name": "Lou Ritter",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+    "scores": [
+    "4",
+    "3",
+    "4",
+    "1",
+    "5",
+    "2",
+    "5",
+    "3",
+    "1",
+    "4"
+    ]
+    },
+    {
+    "name": "Jordan Biason",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
+    "scores": [
+    "4",
+    "4",
+    "2",
+    "3",
+    "2",
+    "2",
+    "3",
+    "2",
+    "4",
+    "5"
+    ]
+    },
+    {
+    "name": "Zorro",
+    "photo": "Zorro",
+    "scores": [
+    "3",
+    "2",
+    "1",
+    "5",
+    "2",
+    "3",
+    "5",
+    "2",
+    "2",
+    "1"
+    ]
+    },
+    {
+    "name": "frank",
+    "photo": "https://unsplash.com/photos/kml8ToIqWN0",
+    "scores": [
+    "2",
+    "2",
+    "2",
+    "2",
+    "2",
+    "2",
+    "2",
+    "2",
+    "2",
+    "2"
+    ]
+    },
+    {
+    "name": "cray",
+    "photo": "there",
+    "scores": [
+    "1",
+    "1",
+    "2",
+    "2",
+    "3",
+    "3",
+    "4",
+    "4",
+    "5",
+    "5"
+    ]
+    },
+    {
+    "name": "Adam G",
+    "photo": "link",
+    "scores": [
+    "4",
+    "3",
+    "4",
+    "1",
+    "4",
+    "2",
+    "3",
+    "2",
+    "2",
+    "2"
+    ]
+    },
+    {
+    "name": "ron",
+    "photo": "https://pbs.twimg.com/profile_images/758084549821730820/_HYHtD8F.jpg",
+    "scores": [
+    "4",
+    "1",
+    "3",
+    "2",
+    "5",
+    "4",
+    "3",
+    "2",
+    "4",
+    "2"
+    ]
+    },
+    {
+    "name": "Lukas Saeger Simianer",
+    "photo": "https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/65842815_710313389410051_867415125563277312_n.jpg?_nc_cat=110&_nc_oc=AQnrrmuTzndXOriGnNAXT9RFwIXMQitDZoojJDmEaphO0N8VTQPq0ObNU3uXU8tdeWU&_nc_ht=scontent-dfw5-1.xx&oh=86c7a827d3d1796aa9593035dccb82cf&oe=5DB7AA89",
+    "scores": [
+    "5",
+    "3",
+    "3",
+    "1",
+    "2",
+    "4",
+    "5",
+    "4",
+    "1",
+    "2"
+    ]
+    },
+    {
+    "name": "blahblah",
+    "photo": "https://images.unsplash.com/photo-1556227703-d6f677369617?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80",
+    "scores": [
+    "2",
+    "3",
+    "4",
+    "1",
+    "4",
+    "3",
+    "5",
+    "1",
+    "2",
+    "4"
+    ]
+    },
+    {
+    "name": "rg",
+    "photo": "rqg",
+    "scores": [
+    "2",
+    "4",
+    "3",
+    "4",
+    "3",
+    "3",
+    "4",
+    "3",
+    "3",
+    "3"
+    ]
+    },
+    {
+    "name": "Vince",
+    "photo": "https://scontent.ffcm1-2.fna.fbcdn.net/v/t1.0-9/56541952_10157374805476661_7797106572873695232_n.jpg?_nc_cat=106&_nc_oc=AQlH77YkfBYInLusJ_-aVanO6dMhLb1AFja1hXV9hVVlcFfGp1UPgGjWMvVziP-1eK0&_nc_ht=scontent.ffcm1-2.fna&oh=f7b9e6b83aad1c8d70c86d5fb6d0c7a0&oe=5DB1E6CE",
+    "scores": [
+    "4",
+    "4",
+    "4",
+    "2",
+    "4",
+    "1",
+    "3",
+    "2",
+    "3",
+    "3"
+    ]
+    },
+    {
+    "name": "john",
+    "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpOvBsfgg3sWH7aFI6iq5N6_Cm5eywuhiYtKBUG_c5HT1TwlZ1",
+    "scores": [
+    "1",
+    "3",
+    "1",
+    "2",
+    "3",
+    "3",
+    "4",
+    "5",
+    "4",
+    "4"
+    ]
+    },
+    {
+    "name": "Garry",
+    "photo": "https://bit.ly/2XwW1Bv",
+    "scores": [
+    "4",
+    "3",
+    "4",
+    "2",
+    "5",
+    "1",
+    "4",
+    "3",
+    "4",
+    "5"
+    ]
+    },
+]
