39cbc50ded23a73c00d6352337132fa33a134714
shar09 <sharathtelu9@gmail.com>
2019-07-09 16:36:32 -0500

    Displaying best match based on scores

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,53 +6,55 @@ module.exports = function (app) {
     });
 
     app.post("/api/friends", function(req, res) {
-    var newFriend = req.body;
-    console.log(newFriend);
+        var newFriend = req.body;
+        console.log(newFriend);
 
-    //scores of new user
-    var newScores = req.body.scores;
-    console.log(newScores);
-    //sum diff of scores of new user and existing user
-    var scoresArr = [];
-    var bestMatch = 0;
-    try {
-        for(i=0; i < friends.length; i++) {
-            var scoreDiff = 0;
-            for(j=0; j < newScores.length; i++) {
-                scoreDiff = scoreDiff + Math.abs(parseInt(friends[i].scores[j]-parseInt(newScores[j])));
+        //scores of new user
+        var newScores = req.body.scores;
+        console.log(newScores);
+        //sum diff of scores of new user and existing user
+        var scoresArr = [];
+        var bestMatch = 0;
+        
+        try {
+            for(i=0; i < friends.length; i++) {
+                var scoreDiff = 0;
+                for(j=0; j < newScores.length; j++) {
+                    scoreDiff = scoreDiff + Math.abs(parseInt(friends[i].scores[j]-parseInt(newScores[j])));
+                }
+                scoresArr.push(scoreDiff);
+                console.log("Scores array:" +scoresArr);
             }
+        }    
+        
+        catch(e) {
+            console.log("error", e);
         }
-    }    
-    
-    catch(e) {
-        console.log("error", e);
-    }
 
-    scoresArr.push(scoreDiff);
-    console.log("Scores array:" +scoresArr);
+        scoresArr.push(scoreDiff);
+        console.log("Scores array:" +scoresArr);
 
-    for(i=0; i < scoresArr.length; i++) {
-        if(scoresArr[i] <= scoresArr[bestMatch])
-        bestMatch = i;
-    }
+        for(i=0; i < scoresArr.length; i++) {
+            if(scoresArr[i] <= scoresArr[bestMatch])
+            bestMatch = i;
+        }
 
-    var soulMate = friends[bestMatch];
-    res.json(soulMate.name);
-    console.log(soulMate);
+        var soulMate = friends[bestMatch];
+        res.json(soulMate);
+        console.log(soulMate);
+        
+        //pushing new user to the friends object
+        friends.push(newFriend);
+        //res.json(newFriend);
 
-    //pushing new user to the friends object
-    friends.push(newFriend);
-    res.json(newFriend);
+        // var bestMatch = {
+        //     name: "",
+        //     photo: "",
+        // };
 
-
-    // var bestMatch = {
-    //     name: "",
-    //     photo: "",
-    // };
-
-    // var friend = newFriend.name;
-    // var friendScores = newFriend.scores;
-    
-    
-});
+        // var friend = newFriend.name;
+        // var friendScores = newFriend.scores;
+        
+        
+    });
 }
